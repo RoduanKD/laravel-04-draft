6e509ca63d44bad2eb0e8ecd6a98ae49485d9e97
Anwar Barakat <84292092+Anwar-Barakat@users.noreply.github.com>
2021-10-28 00:40:24 +0300

    create , edit , index , show edit
    some changes on these files

---
+++ edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('title', '- edit: ' . $tag->name)
+
+@section('content')
+    <section class="section" style="    height: 69vh;">
+        <div class="container">
+            <form action="{{ route('tags.update',$tag) }}" method="POST">
+                @method('PUT')
+                @csrf
+                <div class="columns is-centered">
+                    <div class="column is-6">
+                        <div class="columns is-multiline is-vcentered">
+                            <div class="column is-12">
+                                <h2 class="title is-2">Edit tag</h2>
+                            </div>
+                            <div class="column is-12">
+                                <div class="field">
+                                    <label class="label">name</label>
+                                    <div class="control">
+                                        <input class="input @error('name') is-danger @enderror" name="name" type="text"
+                                            placeholder="Post name"
+                                            value="{{ $tag->name }}">
+                                    </div>
+                                    @error('name')
+                                        <p class="help is-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="column is-12">
+                                <input  type="submit"
+                                        class="button is-primary is-outlined is-fullwidth"
+                                        value="Add new tag">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+@endsection
