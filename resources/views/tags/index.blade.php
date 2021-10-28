32060c046705e3f4514ce28ba89fb7b16422f862
Anwar Barakat <84292092+Anwar-Barakat@users.noreply.github.com>
2021-10-28 18:22:05 +0300

    edit , show , index , create tag

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -3,13 +3,20 @@
 @section('title', '- Tags')
 
 @section('content')
-    <h3>Tags</h3>
-    <h6><a href="{{ route('tags.create') }}">Add new tag</a></h6>
-    @foreach ($tags as $tag)
-        <div>
-            <a href="{{ route('tags.show', $tag) }}">
-                <h4>{{ $tag->name }}</h4>
-            </a>
+    <section class="section">
+        <div class="container">
+            <div class="column is-12 block">
+                <h2 class="title is-2">All tags</h2>
+            </div>
+            <div class="columns" style="display: flex;justify-content: space-between;flex-wrap: wrap;padding: 1.5rem;">
+                @foreach ($tags as $tag)
+                    <div class="button column is-3 is-primary is-light is-outlined">
+                            <a href="{{ route('tags.show', $tag) }}">
+                                <h4>{{ $tag->name }}</h4>
+                            </a>
+                    </div>
+                @endforeach
+            </div>
         </div>
-    @endforeach
+    </section>
 @endsection
