0b700a30a2ee6044b530755f36736660b4d8811e
alaa-unique <alaa.unique@gmail.com>
2021-10-28 19:38:39 +0300

    alaa

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,12 +3,29 @@
 @section('title', '- Create new Category')
 
 @section('content')
-    <h2>Create New category</h2>
-    <form action="{{ route('categories.store') }}" method="POST">
-        @csrf
-        <label>
-            Name
-            <input type="text" name="name">
-        </label>
-    </form>
-@endsection
+    <section class="section">
+        <div class="container">
+            <div class="coulmns is-centered is-multiline">
+                <div class="coulmn is -10">
+                    <h2 class="title is-2">Create New category</h2>
+                    <form action="{{ route('categories.store') }}" method="POST">
+                        @csrf
+                        <div class="field">
+                            <label class="label">Name</label>
+                            <div class="control">
+                                <input class="input @error('name') is-danger @enderror" name="name" type="text"
+                                    placeholder="categories Title" value="{{ old('name') }}">
+                            </div>
+                            @error('name')
+                                <p class="help is-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="column is-12"><input type="submit"
+                            class="button is-primary is-outlined is-fullwidth" value="Add new Categories">
+                    </div>
+                    </form>
+                    @endsection
+                </div>
+            </div>
+        </div>
+    </section>
--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -3,13 +3,37 @@
 @section('title', '- Tags')
 
 @section('content')
-    <h3>Tags</h3>
-    <h6><a href="{{ route('tags.create') }}">Add new tag</a></h6>
-    @foreach ($tags as $tag)
-        <div>
-            <a href="{{ route('tags.show', $tag) }}">
-                <h4>{{ $tag->name }}</h4>
-            </a>
+<section class="hero is-info is-halfheight">
+    <div class="hero-body">
+        <div class="container">
+            <div class="">
+                <p class="title">
+                    Half height hero
+                </p>
+                <p class="subtitle">
+                    Half height subtitle
+                </p>
+            </div>
         </div>
-    @endforeach
-@endsection
+    </div>
+</section>
+<section class="section">
+        <div class="container">
+            <div >
+                <div >
+                    <h3  class="title  is-2">Tags</h3><br>
+                </div>
+            </div>
+            <div class="level-right">
+            <h6 class="subtitle is 6"><a class ="button is-primary is-outlined is-light" href="{{ route('tags.create') }}">Add new tag</a></h6>
+            </div>
+            @foreach ($tags as $tag)
+                <div class="columns is multiline">
+                    <a href="{{ route('tags.show', $tag) }}">
+                        <h4 class="title is-4">{{ $tag->name }}</h4>
+                    </a>
+                </div>
+        </div>
+        @endforeach
+    @endsection
+</section>
