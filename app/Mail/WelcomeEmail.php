9873d5bf930d09ba37d324edf3c4c4e2aa890ddb
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-11-16 19:27:51 +0200

    add queues to mails & notifications

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class WelcomeEmail extends Mailable
+class WelcomeEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
